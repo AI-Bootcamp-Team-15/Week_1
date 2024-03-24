4f846ddce20267b009a9efeed350bcd8944d9618
Archeks <achikotsi@gmail.com>
2024-03-24 23:21:53 +0000

    Added main.py and base class for ChefGPT

---
+++ BaseChefGPT.py
@@ -0,0 +1,48 @@
+from openai import OpenAI
+
+class BaseChefGPT:
+    def __init__(self):        
+        self.client = OpenAI()
+        self.messages = []
+        self.name = "Base chef"
+
+    def run(self, user_input = None):        
+        if user_input is None:
+            print("\n")
+            user_input = input("How can " + self.name + " assist you today?\n")
+    
+        model = "gpt-3.5-turbo"
+
+        while True:
+            self.messages.append(
+                {
+                    "role": "user",
+                    "content": user_input
+                }
+            )
+
+            stream = self.client.chat.completions.create(
+                model=model,
+                messages=self.messages,
+                stream=True,
+            )
+            collected_messages = []
+            for chunk in stream:
+                chunk_message = chunk.choices[0].delta.content or ""
+                print(chunk_message, end="")
+                collected_messages.append(chunk_message)
+            
+            collected_string = "".join(collected_messages)
+            self.messages.append(
+                {
+                    "role": "system",
+                    "content": collected_string
+                }
+            )
+            if (not "try again" in collected_string.lower()):
+                break
+            else:
+                user_input = input()
+
+                
+        return collected_string
---
+++ main.py
@@ -0,0 +1,19 @@
+from ArcheksChef import ArcheksChef
+
+chefs = [ArcheksChef()]
+
+
+while True:
+    print("\n")
+    for i in range(len(chefs)):
+        print(i, '-', chefs[i].name)
+
+    print('Type "exit" to terminate script\n')
+
+    chefNumber = input("Select GPT Chef by number:\n")
+    if  chefNumber.isnumeric() and int(chefNumber) in range(len(chefs)):
+        chefs[i].run()
+    else:
+        if chefNumber=="exit":
+            break;
+        print("Wrong number\n")
